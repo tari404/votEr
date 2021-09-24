f153e3ae060abb91e37f3f1f31ae0f11a2608537
tari404 <2680383605@qq.com>
2021-09-24 23:16:10 +0800

    📦 update title

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: './',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title = 'Froggies'
+      args[0].title = 'votEr'
       return args
     })
   },
